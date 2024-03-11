016acb65c8e66cc067887c3ea4fd829c3ccf87cb
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-03-11 17:48:09 +0000

    Cambios de generador de excusa

--- src/app.js
+++ src/app.js
@@ -8,4 +8,38 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  // Función para generar excusa aleatoria
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    // Obtener índices aleatorios para cada arreglo
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    // Construir la excusa aleatoria
+    let excuse =
+      who[whoIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex];
+
+    return excuse;
+  }
+
+  console.log(generateExcuse());
 };
